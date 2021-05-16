7685f1a4a558fd0d580efce9d1b91363a7b9fd45
kajiki71 <gt.kajiki71@gmail.com>
2021-05-16 14:52:51 +0900

    Upload new file.

---
+++ app/src/main/java/dotinstall/com/MainActivity.kt
@@ -0,0 +1,17 @@
+package dotinstall.com
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun changeTextView(view: View) {
+        messageTextView.text="Hellow there!"
+
+    }
+}
